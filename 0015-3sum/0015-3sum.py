3f069ac6c537aab4c9102a7a8e140fa1a21ab8cc
Kate <kaycxli@gmail.com>
2026-05-03 16:38:08 -0400

    Time: 3684 ms (5.01%), Space: 498.6 MB (5.18%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,40 @@
+'''
+maybe one for loop and within it do the normal two sum, to remove duplicates
+only scan forward each time (no going backwards) --- wrote this out and 
+it exceeds memory limit 
+use a hashmap to map keys (two sum) to their values (two unique indexes) 
+then for every num in nums see if there is a key in th map that adds to 0, 
+and if the index of num is not in the value, append to solution 
+
+oops forgot how to use hashmaps again lmao had to relearn the syntax
+
+'''
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        # sort array 
+        nums.sort()
+        sol = []
+
+        # for every int i in array, want to find two corresponding int j and k 
+        # such that j + k = -i. Achieve this using the two sum ii approach 
+
+        for i, num in enumerate(nums):
+            l = i + 1
+            r = len(nums) - 1
+            while (l < r):
+                total = nums[l] + nums[r]
+
+                if (total == -num):
+                    sol.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                elif (total > -num):
+                    r -= 1
+                else:
+                    l += 1 
+
+        # remove duplicates
+        for s in sol:
+            s.sort()
+        return [list(x) for x in (set(tuple(y) for y in sol))]
+
